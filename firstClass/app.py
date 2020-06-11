b3a4a23b07bb0662df48d5b69fb04cbc95597242
mlynch018 <64503572+mlynch018@users.noreply.github.com>
2020-06-11 14:18:43 -0500

    First Python Class

---
+++ firstClass/app.py
@@ -0,0 +1,9 @@
+#first student file, second class
+from Student import Student
+
+#note booleans start with uppercase letters
+student1 = Student("Jim", "Business", 3.1, False)
+student2 = Student("Pam", "Art", 2.5, True)
+
+print(student1.name)
+print(student2.gpa)
